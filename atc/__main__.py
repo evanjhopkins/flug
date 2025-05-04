44f9e283cb4ce2bcab07773fe9c09fb4e001bd3e
Evan Hopkins <evanjhopkins@yahoo.com>
2025-05-03 21:16:48 -0400

    test commands

--- atc/__main__.py
+++ atc/__main__.py
@@ -1,5 +1,31 @@
-def main():
-    print("Hello from atc CLI")
+import click
+import yaml
+from lib.db_actions import assert_db_initialized, nuke_db
+import os
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+def status():
+    assert_db_initialized()
+    click.echo("Status")
+
+@click.command()
+def nuke():
+    nuke_db()
+
+@click.command()
+@click.argument('file_path', type=click.Path(exists=True, dir_okay=False))
+def register(file_path):
+    print(os.getcwd())
+    abs_path = os.path.abspath(file_path)
+    print(abs_path)
+
+cli.add_command(status)
+cli.add_command(nuke)
+cli.add_command(register)
 
 if __name__ == "__main__":
-    main()
+    cli()
---
+++ atc/service.py
@@ -0,0 +1,11 @@
+import time
+
+def start():
+	print("Starting ATC Service")
+	while True:
+		print("tick")
+		time.sleep(1)
+
+
+if __name__ == "__main__":
+	start()
